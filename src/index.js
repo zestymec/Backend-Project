5bc5bd1f027eb3c8caa4b85b51a975c0ec52202d
umer <m.umer394aziz@gmail.com>
2026-06-17 04:26:10 +0500

    mongodb connected and db handling

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv"
+dotenv.config({ path: "./.env" })
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+connectDB()
