3ae00dc984fa2b42d33bc2c9a8a042e28ad7d095
umer <m.umer394aziz@gmail.com>
2026-06-17 04:57:50 +0500

    app.js and idex updated for db

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 
 connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
