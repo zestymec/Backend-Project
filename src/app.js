e228c010d5413e1a8b0a1d972b3d66b6399a16da
zestymec <m.umer394aziz@gmail.com>
2026-06-17 11:01:11 +0500

    packages installed and api errors and response and async handler added in utils

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
-import express from "express"
+import express, { urlencoded } from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import { cookie } from "express/lib/response"
 
 const app = express()
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN ,
+    credentials : true ,
+}))
+
+app.use(express.json({
+    limit: "10kb"
+}))
+
+app.use(express.static("Pulic"))
+
+app.use(cookieParser())
+
+app.use(urlencoded({extended:true , limit : "12kb"}))
+export {app}
